4fe9a2b2d07deaa976e445069639d07bd50bdfec
ric-rhee <rheer@purdue.edu>
2021-09-29 21:36:17 -0400

    testing

--- get_metadata.py
+++ get_metadata.py
@@ -1,8 +1,6 @@
 import requests
 from pprint import pprint
 
-username = "ric-rhee"
-url = f"https://api.github.com/users/{username}"
-#url = f"https://api.github.com/nullivex/nodist"
-user_data = requests.get(url).json()
-pprint(user_data)
+url = f"https://api.github.com/repos/nullivex/nodist/commits"
+data = requests.get(url).json()
+pprint(data)
