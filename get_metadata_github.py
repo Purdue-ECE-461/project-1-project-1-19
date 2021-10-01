45d1cff04b433938e8a4f48df31316bc1d8a1c5c
ric-rhee <rheer@purdue.edu>
2021-09-30 23:22:40 -0400

    saving

--- get_metadata_github.py
+++ get_metadata_github.py
@@ -1,6 +1,21 @@
 import requests
 from pprint import pprint
 
-url = f"https://api.github.com/repos/nullivex/nodist/commits"
-data = requests.get(url).json()
-pprint(data)
+
+filename = "tests/test1.txt"
+urls = []
+data = []
+with open(filename) as f:
+    urls = f.read().splitlines()
+
+for url in urls:
+    ind = url.find("github")
+    front = url[:ind]
+    back = url[ind:]
+    url = front + "api." + back
+    print(url)
+
+
+
+# number of contributors (in the last year)
+# number of open/closed issues
